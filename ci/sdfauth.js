0c9255e0ff32269de30db9d50e1399a30150d276
akashc026 <akashc026@gmail.com>
2022-08-29 18:44:21 +0530

    new changes123'

--- ci/sdfauth.js
+++ ci/sdfauth.js
@@ -1,19 +1,19 @@
 const { exec } = require("child_process");
-const realm = process.env.realm_master;
-const token = process.env.token_master;
-const secret = process.env.secret_master;
+const realm = process.env.REALM_TECHNICAL;
+const token = process.env.TOKEN_TECHNICAL;
+const secret = process.env.SECRET_TECHNICAL;
 
 const real_gapsol = process.env.real_gapsol;
 const token_gapsol = process.env.token_gapsol;
 const secret_gapsol = process.env.secret_gapsol;
-const branch = process.env.CI_COMMIT_BRANCH;
-if(branch == "master")
-{
-var authCmd = `suitecloud account:savetoken --account ${real_gapsol} --authid "cisdf" --tokenid ${token_gapsol} --tokensecret ${secret_gapsol}`;
-}
-else{
+//const branch = process.env.CI_COMMIT_BRANCH;
+// if(branch == "master")
+// {
+// var authCmd = `suitecloud account:savetoken --account ${real_gapsol} --authid "cisdf" --tokenid ${token_gapsol} --tokensecret ${secret_gapsol}`;
+// }
+// else{
   var authCmd = `suitecloud account:savetoken --account ${realm} --authid "cisdf" --tokenid ${token} --tokensecret ${secret}`;
-}
+//}
 
 exec(authCmd, realm, (error, stdout, stderr) => {
   if (error) {
